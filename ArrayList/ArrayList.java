849950445d32415b6b0291c58e6d59947e12934d
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-04 12:08:38 +0900

    Update ArrayList.java

--- ArrayList/ArrayList.java
+++ ArrayList/ArrayList.java
@@ -1,1 +1,18 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        a.add("안녕하세요");
+        a.add("반갑습니다");
+        a.add("자바");
+        a.add(2, "c언어");
+
+        System.out.println(a.get(1));
+        System.out.println(a.size());
+        System.out.println(a.remove(2) + " 삭제하겠습니다");
+    }
+}
 
