5da90717c50f8f3f066ac1e2d9ee633046418b80
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-06 22:08:34 +0900

    Update GenericEx2.java

--- GenericClass/GenericEx2.java
+++ GenericClass/GenericEx2.java
@@ -6,21 +6,31 @@ public class GenericStackEx<T> {
 
     public GenericStackEx() {
         index = 0;
-        stack = new Object[10];
+        stack = new Object[10]; //제네틱 타입 객체 생성 불가 그래서! Object객체 생성
     }
+    /** pushStack Method
+     * index 0부터 value를 저장함
+     * 만약 stack 크기보다 크면 경고메세지 출력
+     * 예외처리 해야되지만 귀찮아요~
+     */
     public void pushStack(T value) {
         stack[index] = value;
         index++;
-        if (index > 10) {
+        if (index > stack.length) {
             System.out.println("스택 용량 꽉참 못넣는다.");
         }
     }
+    /** popStack Method
+     * 저장한 마지막 값 부터 꺼내와요~
+     * 예외처리 해야되지만 귀찮아요~
+     * Casting 주의!
+     */
     public void popStack() {
         if (index == 0) {
             System.out.println("비어있습니다. 못 꺼내요..");
         }
         index --;
-        System.out.println((T)stack[index]);
+        System.out.println((T)stack[index]); //Casting 해야됨 Object 타입 -> T으로
     }
 
     public static void main(String[] args) {
