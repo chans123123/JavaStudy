4ab40e3834c125f2d4215934525f268129c0b604
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-06 21:39:39 +0900

    Update GenericEx.java

--- GenericClass/GenericEx.java
+++ GenericClass/GenericEx.java
@@ -10,10 +10,10 @@ public class MyClass<T> {
     }
 
     public static void main(String[] args) {
-        MyClass<String> stringMyClass = new MyClass<>();
+        MyClass<String> stringMyClass = new MyClass<>(); // T를 String으로 구체화 시킴
         stringMyClass.setValue("Hello");
         stringMyClass.getValue();
-        MyClass<Integer> integerMyClass = new MyClass<>();
+        MyClass<Integer> integerMyClass = new MyClass<>(); // T를 Integer으로 구체화 시킴
         integerMyClass.setValue(5);
         stringMyClass.getValue();
     }
