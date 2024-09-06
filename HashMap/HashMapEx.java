f3bb5c3a1b222bbc4c68ff3216e529041c472001
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-06 22:15:33 +0900

    Update HashMapEx.java

--- HashMap/HashMapEx.java
+++ HashMap/HashMapEx.java
@@ -1,1 +1,21 @@
+package HashMap;
+
+import java.util.HashMap;
+
+public class Hash_Map {
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("apple", "사과"); // key: apple value: 사과
+        hashMap.put("banana", "바나나"); // key: banana value: 바나나
+        hashMap.put("lemon", "레몬"); // key : 레몬 value: 레몬
+
+        String kor = hashMap.get("apple");
+        String kor2 = hashMap.get("banana");
+        String kor3 = hashMap.get("lemon");
+
+        System.out.println(kor);
+        System.out.println(kor2);
+        System.out.println(kor3);
+    }
+}
 
