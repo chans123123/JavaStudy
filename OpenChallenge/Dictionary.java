f9958e82f3f1690106c9476a6e4c32c378e7f001
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-07 17:56:35 +0900

    Update Dictionary.java

--- OpenChallenge/Dictionary.java
+++ OpenChallenge/Dictionary.java
@@ -1,1 +1,12 @@
+public class Dictionary {
+    private String word;
+    private Scanner scanner = new Scanner(System.in);
+    private Vector<Word> vector = new Vector<>();
 
+    public Dictionary(String word) {
+        this.word = word;
+        vector.add(new Word("사진", "photo"));
+        vector.add(new Word("사람", "human"));
+        vector.add(new Word("게임", "game"));
+        vector.add(new Word("캐릭터", "character"));
+    }//Dictionary end
