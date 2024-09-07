874d94deb866692c852b62d5819d2a42252bcae1
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-07 17:56:42 +0900

    Update Dictionary.java

--- OpenChallenge/Dictionary.java
+++ OpenChallenge/Dictionary.java
@@ -10,3 +10,9 @@ public class Dictionary {
         vector.add(new Word("게임", "game"));
         vector.add(new Word("캐릭터", "character"));
     }//Dictionary end
+    public void run() {
+        System.out.println(word + "에 오신걸 환영합니다!" + vector.size() + "개의 단어 테스트를 시작합니다!");
+        System.out.println("-1를 누르면 종료됩니다!");
+        System.out.println("현제 " + vector.size() + "개의 단어가들어가 잇습니다.");
+        QuizStart();
+    } // run end
