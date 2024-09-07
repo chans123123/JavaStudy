5598c7e2530f5d0268b6e59e9d7d02c0122d84be
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-07 17:57:58 +0900

    Create Word.java

---
+++ OpenChallenge/Word/Word.java
@@ -0,0 +1,17 @@
+package OpenChanllenge.Word;
+
+public class Word {
+    private String key;
+    private String value;
+
+    public Word(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+    public String getKey() {
+        return key;
+    }
+    public String getValue() {
+        return value;
+    }
+}
