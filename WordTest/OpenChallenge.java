f382a2ab5a64eead030d7018e51b0f35e620879c
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-07 18:02:12 +0900

    Add files via upload

---
+++ WordTest/OpenChallenge.java
@@ -0,0 +1,10 @@
+package OpenChanllenge;
+
+import OpenChanllenge.Dictionary.Dictionary;
+
+public class OpenChallenge {
+    public static void main(String[] args) {
+        Dictionary dictionaray = new Dictionary("안녕하세요");
+        dictionaray.run();
+    }
+}
