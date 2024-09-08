344af9b64668b261f70fa36811f324c5513910e2
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-08 22:40:15 +0900

    Update GenericEx3.java

--- GenericClass/GenericEx3.java
+++ GenericClass/GenericEx3.java
@@ -1,1 +1,14 @@
+import Stack.Stack;
 
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> integerStack = new Stack<Integer>();
+        for (int i = 0; i < 5; i++) {
+            integerStack.push(i);
+        }
+        integerStack = reverseStack(integerStack);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(integerStack.pop());
+        }
+    }
