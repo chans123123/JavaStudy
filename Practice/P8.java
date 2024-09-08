ec7e0d184eeb5f8f7982ee698f7aaeb619a56d2c
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-08 22:42:16 +0900

    Update P8.java

--- Practice/P8.java
+++ Practice/P8.java
@@ -1,1 +1,13 @@
+package Ptactice;
 
+import java.util.ArrayList;
+
+public class P8 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>(10);
+        arrayList.add("Java");
+        arrayList.add("C++");
+        System.out.println(arrayList.size());
+        arrayList.remove(arrayList.size()-1);
+    }
+}
