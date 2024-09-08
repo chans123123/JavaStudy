7d71e0c59e74f645e9bd8a1cd43d870a6d687ff5
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-08 22:42:43 +0900

    Update P9.java

--- Practice/P9.java
+++ Practice/P9.java
@@ -1,1 +1,18 @@
+package Ptactice;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+public class P9 {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+        for (int i = 0; i < 5; i++) {
+            vector.add(i);
+        }
+        Iterator<Integer> iterator = vector.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next() + " ");
+        }
+    }
+}
 
