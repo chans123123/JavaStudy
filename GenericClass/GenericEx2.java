5bb32c26aac0b9377b310153b9a98e6ec51b8b42
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-08 22:41:40 +0900

    Update GenericEx2.java

--- GenericClass/GenericEx2.java
+++ GenericClass/GenericEx2.java
@@ -53,4 +53,18 @@ public class GenericStackEx<T> {
             genericStackEx.popStack();
         }
     }
+    public static <T> Stack<T> reverseStack(Stack<T> stack) {
+        Stack<T> stack1 = new Stack<T>();
+        while (true) {
+            T temp;
+            temp = stack1.pop();
+            if (temp == null) {
+                break;
+            } //if end
+            else {
+                stack1.push(temp);
+            } // else end
+        }//while end
+        return stack1;
+    }    
 }
