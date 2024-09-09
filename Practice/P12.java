0d56dcf6533faea2d9bee054ff5d95f30cde56e6
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-09 21:56:30 +0900

    Update P12.java

--- Practice/P12.java
+++ Practice/P12.java
@@ -1,1 +1,13 @@
+package Ptactice;
+
+public class P12 {
+    //2
+    static <T> T take(T s[], int index) {
+        if (index > s.length) {
+            System.out.println("인덱스 범위 초과");
+            return null;
+        }
+        return s[index];
+    }
+}
 
