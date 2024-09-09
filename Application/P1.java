c696d84f644b6a9c7a58a520a12ccc6d6c27a937
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-09 21:57:17 +0900

    Update P1.java

--- Application/P1.java
+++ Application/P1.java
@@ -1,1 +1,22 @@
+package Application;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class P1 {
+    public static void main(String[] args) {
+        Vector<Integer> vector = new Vector<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("정수(-1이 입력될 때가지)>> ");
+        while (true) {
+            int input = scanner.nextInt();
+            vector.add(input);
+            if (input == -1) {
+                break;
+            } //if end
+        }// while end
+        System.out.println("가장 큰 수는 " + Collections.max(vector));
+    }
+}
 
