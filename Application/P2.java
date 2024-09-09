daaff4de9e4788e678dbbdf7c2e65ea740ce2741
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-09 21:57:49 +0900

    Create P2.java

---
+++ Application/P2.java
@@ -0,0 +1,11 @@
+package Application;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P2 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("6개의 학점을 빈 칸으로 분리 입력(A/B/C/D/F)>>");
+        double total_score = 0;
