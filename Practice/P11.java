89e81b90fb2a8b0e3dc71756cefbc19e91f08987
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-09 21:56:04 +0900

    Update P11.java

--- Practice/P11.java
+++ Practice/P11.java
@@ -1,1 +1,15 @@
+package Ptactice;
+import java.util.HashMap;
 
+public class P10 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        hashMap.put("a", 10);
+        hashMap.put("b", 11);
+        System.out.println(func(hashMap));
+    }
+
+    public static int func(HashMap<String, Integer> hashMap) {
+        return hashMap.size();
+    }
+}
