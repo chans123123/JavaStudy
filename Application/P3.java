c5c28fd380b1d2ba29f9fb8261daaff8b593369d
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-09 22:17:50 +0900

    Update P3.java

--- Application/P3.java
+++ Application/P3.java
@@ -1,1 +1,16 @@
+package Application;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        System.out.println("나라 이름과 인구를 입력하세요.(예: Korea 5000)");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("나라 이름, 인구>> ");
+            String nation = scanner.next();
+            if (nation.equals("그만")) {
+                break;
+            }
