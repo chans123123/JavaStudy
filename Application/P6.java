b6b6d4914e04e2e53977ab24fb5c30c43307fee5
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-10 20:14:28 +0900

    Update P6.java

--- Application/P6.java
+++ Application/P6.java
@@ -21,3 +21,18 @@ class Location {
         return wee;
     }
 }
+public class P6 {
+    public static void main(String[] args) throws IOException {
+        System.out.println("도시, 경도, 위도를 입력하세요");
+        HashMap<String, Location> hashMap = new HashMap<>();
+        for (int i = 0; i < 4; i++) {
+            System.out.print(">>");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+            String city = st.nextToken().trim();
+            int gyeong = Integer.parseInt(st.nextToken().trim());
+            int wee = Integer.parseInt(st.nextToken().trim());
+
+            Location location = new Location(gyeong, wee);
+            hashMap.put(city, location);
+        }
