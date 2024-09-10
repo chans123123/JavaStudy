a8688fab5d3b387a35cb550fb8c5112189119032
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-10 20:14:21 +0900

    Update P6.java

--- Application/P6.java
+++ Application/P6.java
@@ -1,1 +1,23 @@
+package Generic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+class Location {
+    private int gyoeng, wee;
+    public Location(int gyoeng, int wee) {
+        this.gyoeng = gyoeng;
+        this.wee = wee;
+    }
+    public int getGyoeng() {
+        return gyoeng;
+    }
+    public int getWee() {
+        return wee;
+    }
+}
