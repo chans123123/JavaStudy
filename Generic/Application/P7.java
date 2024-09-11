afb95a631e591beaf8c995fcd9f0533d3442bf9a
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-11 21:02:27 +0900

    Update P7.java

--- Generic/Application/P7.java
+++ Generic/Application/P7.java
@@ -1,1 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.StringTokenizer;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        HashMap<String, Double> hashMap = new HashMap<>();
+        System.out.println("미래장학금관리시스템입니다.");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("이름과 학점>> ");
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            Double avg = Double.parseDouble(st.nextToken());
+            hashMap.put(name, avg);
+        }
