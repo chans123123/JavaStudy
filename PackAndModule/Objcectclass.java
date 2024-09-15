15534d214ced205756e00a10cba526ebba0afe22
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-15 23:02:45 +0900

    Update Objcectclass.java

--- PackAndModule/Objcectclass.java
+++ PackAndModule/Objcectclass.java
@@ -5,3 +5,10 @@ class Point {
     this.y = y;
   }
 }
+public class ObjectPropertyEx {
+  public static void print(Object obj) {
+    System.out.println(obj.getClass().getName()); // 클래스 이름
+    System.out.println(obj.hashCode()); // 해시 코드 값
+    System.out.println(obj.toString()); // 객체를 문자열로 만들어 출력
+    System.out.println(obj); // 객체 출력
+  }
