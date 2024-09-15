3292f177e8397b2a95cf1a4beda02d5294e9fb1c
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-15 23:03:07 +0900

    Update Objcectclass.java

--- PackAndModule/Objcectclass.java
+++ PackAndModule/Objcectclass.java
@@ -12,3 +12,8 @@ public class ObjectPropertyEx {
     System.out.println(obj.toString()); // 객체를 문자열로 만들어 출력
     System.out.println(obj); // 객체 출력
   }
+  public static void main(String [] args) {
+    Point p = new Point(2,3);
+    print(p);
+  }
+}
