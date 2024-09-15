4905ce8d84b0008887a78485954a708d1fe6c59f
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-15 23:06:12 +0900

    Update String_toString.java

--- PackAndModule/String_toString.java
+++ PackAndModule/String_toString.java
@@ -3,7 +3,7 @@ class Point {
   public Point(int x, int y) {
     this.x = x;
     this.y = y;
-  }
+}
   public String toString() {
     return "Point(" + x + "," + y + ")";
   }
@@ -15,3 +15,4 @@ public class ToStringEx {
     System.out.println(p); // p는 p.toString()으로 자동 변환
     System.out.println(p + "입니다."); // p.toString() + "입니다"로 자동 변환
   }
+}
