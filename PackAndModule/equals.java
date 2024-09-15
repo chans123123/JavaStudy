dff942537f1864a2c9ffe05b4f2e5b25fc9b3fee
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-09-15 23:08:23 +0900

    Update equals.java

--- PackAndModule/equals.java
+++ PackAndModule/equals.java
@@ -11,3 +11,17 @@ public boolean equals(Object obj) {
       return false;
   }
 }
+
+public class EqualsEx {
+  public static void main(String[] args) {
+    Point a = new Point(2,3);
+    Point b = new Point(2,3);
+    Point c = new Point(3,4);
+    if(a == b) // false
+      System.out.println("a==b");
+    if(a.equals(b)) // true
+      System.out.println("a is equal to b");
+    if(a.equals(c)) // false
+      System.out.println("a is equal to c");
+  }
+}
