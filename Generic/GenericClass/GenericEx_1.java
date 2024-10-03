f6fe0df4a94e5b5a85992d22881b09cc72128881
chans123123 <164636385+chans123123@users.noreply.github.com>
2024-10-03 15:06:10 +0900

    Update GenericEx_1.java

--- Generic/GenericClass/GenericEx_1.java
+++ Generic/GenericClass/GenericEx_1.java
@@ -1,1 +1,29 @@
-
+class GStack<T> {
+    int tos;
+    Object [] stck;
+    public GStack() {
+        tos=0;
+        stck = new Object[10]; //제네틱 타입 객체 생성 불가 그래서! Object객체 생성
+    }
+    /** pushStack Method
+     * index 0부터 item을 저장함
+     */
+    public void push(T item) {
+        if (tos == 10) {
+            return;
+        }
+        stck[tos] = item;
+        tos++;
+    }
+    /** popStack Method
+     * 저장한 마지막 값 부터 꺼내옵니다.
+     * Casting 주의!
+     */
+    public T pop() {
+        if (tos ==0) {
+            return null;
+        }
+        tos--;
+        return (T)stck[tos]; //Casting 해야됨 Object 타입 -> T으로 [중요!!!!!!!!!!!!!!!]
+    }
+        }
